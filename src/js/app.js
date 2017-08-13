bcb862e7bd589235f559b5c52789e3b0f8711db0
Ian <ianmilliken@live.co.uk>
2017-08-13 17:06:27 +1000

    ES6 modules for site navigation

--- src/js/app.js
+++ src/js/app.js
@@ -5,48 +5,6 @@
  *
  */
 
-import foo from "./test";
+import siteNavigation from "./site_navigation";
 
-$(document).ready(function() {
-
-   $('.axios-nav-link').click( e => {
-      e.preventDefault()
-      alert("Axios link clicked!")
-   });
-
-   //  -----------------------------
-   //  Hamburger and drawer
-   //  -----------------------------
-
-   var add_classes, close, drawer, hamburger, overlay, remove_classes;
-
-   hamburger = $('#site-burger');
-   overlay = $('.site-overlay');
-   drawer = $('.site-drawer');
-   close = $('.site-drawer__close');
-
-   add_classes = function() {
-     overlay.addClass('site-overlay--active');
-     drawer.addClass('site-drawer--active');
-   };
-
-   remove_classes = function() {
-     overlay.removeClass('site-overlay--active');
-     drawer.removeClass('site-drawer--active');
-   };
-
-   hamburger.off();
-
-   hamburger.on('click', function(event) {
-     event.preventDefault();
-     add_classes();
-   });
-
-   close.off();
-
-   close.on('click', function(event) {
-     event.preventDefault();
-     remove_classes();
-   });
-
-});
+siteNavigation()
--- src/js/test.js
+++
@@ -1,1 +0,0 @@
-export const foo = "THIS IS FOO"
